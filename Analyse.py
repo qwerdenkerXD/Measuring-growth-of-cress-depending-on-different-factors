97535c9b6aeecc821d168b28d98db84cd2054761
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2023-01-31 10:28:11 +0100

    Analyse.py: Defined getMeasurements to get data from Wuchshohen.csv defined constant: CSV_DELIM defined method: main Wuchshohen.csv is now utf-8-only

--- Analyse.py
+++ Analyse.py
@@ -1,4 +1,24 @@
 INIT_DATE = 30, 1, 2023, 10, 27
+CSV_DELIM = ";"
+
+
+def main():
+    pass
+
+
+def getMeasurements():
+    data = {"H2O": [[], []],  # [x-values, y-values]
+            "pH": [[], []],
+            "NaCl": [[], []]
+            }
+    with open("Wuchshohen.csv") as f:
+        f.readline()  # skip first line
+        for line in f:
+            medium, dateTime, height, _ = line.split(CSV_DELIM)
+            data[medium][0] += [relTime(dateTime)]
+            data[medium][1] += [int(height)]
+
+    return data
 
 
 def relTime(dateTime: "dd.mm.yyyy, hh:mm") -> "int of minutes":  # for correct positioning on x-axis
@@ -16,3 +36,7 @@ def relTime(dateTime: "dd.mm.yyyy, hh:mm") -> "int of minutes":  # for correct p
     minutes = int(minutes) + 60 * hours - initMinutes
 
     return minutes
+
+
+if __name__ == '__main__':
+    main()
