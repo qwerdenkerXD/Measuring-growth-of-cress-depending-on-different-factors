e2071b9f89aaeecea89ffbe5dc7fa51bf5c6fb74
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2023-02-02 00:23:38 +0100

    Analyse.py: generates plot for medians and arith. means of the data in Plot.png

--- Analyse.py
+++ Analyse.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plot
+
 INIT_DATE = 30, 1, 2023, 10, 27
 CSV_DELIM = ";"
 
@@ -10,6 +12,37 @@ def main():
     assert len(data["NaCl"][0]) % 20 == 0, "Missing measurement in NaCl"
 
     M_COUNT = len(data["H2O"][0]) // 20  # count of measurements
+
+    arithMean, median = {}, {}
+    for medium in data:  # calculate medians and arith. means of data
+        yArith, yMedian, xValues = [], [], []
+
+        for i in range(M_COUNT):
+            xValues += [data[medium][0][i * 20]]
+            yData = sorted(data[medium][1][i * 20: i * 20 + 20])
+            yArith += [sum(yData) / 20]
+            yMedian += [(yData[9] + yData[10]) / 2]
+
+        arithMean[medium] = [xValues, yArith]
+        median[medium] = [xValues, yMedian]
+
+    # plot
+    MAX_X = data["H2O"][0][-1]
+    MAX_Y = max(data["H2O"][1])
+
+    for i in range(2):
+        subP = plot.subplot(121 + i)
+        subP.title.set_text(["median", "arithmetic mean"][i])
+        subP.set_xticks([i for i in range(0, MAX_X, 60 * 24)])
+        subP.set_xticklabels([i // (60 * 24) for i in range(0, MAX_X, 60 * 24)])
+        subP.set_xlim(0, MAX_X)
+        subP.set_ylim(0, MAX_Y)
+
+        for medium in data:
+            x_values, y_values = eval(["median", "arithMean"][i])[medium]
+            subP.plot(x_values, y_values, marker="o", linestyle="-", label=medium)
+
+    plot.savefig("Plot.png")
 
 
 def getMeasurements() -> dict:
